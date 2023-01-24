dbe60c9a6bd58e1582c964deef881ee9cb29e599
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2023-01-23 20:37:53 -0500

    fdsafdsa

--- api-project/JS/pokemon.js
+++ api-project/JS/pokemon.js
@@ -3,25 +3,30 @@ async function fetchpokemon() {
   for (let i = 1; i < 151; i++) {
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const response = await fetch(url);
+
     if (response.status < 200 || response.status > 299) {
       console.log(response.status);
       throw error(response);
     } else {
       const pokemon = await response.json();
       pokemons.push(pokemon);
-      document.getElementById("display").insertAdjacentHTML(
-        "beforeend",
-
-        `<div data-aos="fade-up" class="display-card2">
-<img class="display-sprite" src="${pokemon.sprites.front_default}" />
-<h4 class="display-name">${pokemon.name}</h4>
-<h5 class="display-type"> type: ${pokemon.types
-          .map((type) => type.type.name)
-          .join(", ")}</h5>`
-      );
+      showpokemon(pokemon);
     }
   }
 }
 fetchpokemon();
+
+function showpokemon(pokemon) {
+  document.getElementById("display").insertAdjacentHTML(
+    "beforeend",
+
+    `<div data-aos="fade-up" class="display-card2">
+<img class="display-sprite" src="${pokemon.sprites.front_default}" />
+<h4 class="display-name">${pokemon.name}</h4>
+<h5 class="display-type"> type: ${pokemon.types
+      .map((type) => type.type.name)
+      .join(", ")}</h5>`
+  );
+}
 
 export { fetchpokemon };
